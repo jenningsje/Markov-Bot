5fde81b9251553496869d16bd1085f1bd3d23973
James <jenningsje@mac.com>
2024-09-13 11:30:24 -0500

    identify working directory in dockerfile

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import util from 'util';
 import path from 'path';
 import { Readable } from "stream";
 
-import { fetch_get } from '../../package.js';
+import { fetch_get } from 'fetch_pdb.js';
 
 export let inferenceProcess: import("child_process").ChildProcessWithoutNullStreams =
   null as any;
