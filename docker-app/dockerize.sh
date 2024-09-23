533e87a8c459942cb1664f9cf85d61bd519b0fe2
James <jenningsje@mac.com>
2024-09-23 11:59:04 -0500

    outsource proprietary logos from outside freedomgpt

--- docker-app/dockerize.sh
+++ docker-app/dockerize.sh
@@ -1,4 +1,6 @@
 cp ../../fetch-pdbs/fetch_pdbs.js ../src/fetch_pdbs.js
+cp ../../logo.png logo.png
+cp ../../logo.png ./src/app/components/logo.png
 docker build -t freedomgpt/freedomgpt .
 docker run -d -p 8889:8889 freedomgpt/freedomgpt
 rm ../src/fetch_pdbs.js
