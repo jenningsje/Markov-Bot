0cc6f6ab2a5c5b213072a2f2378a518308d4b5e1
James <jenningsje@mac.com>
2024-09-27 14:59:07 -0500

    change fetch_pdb to fetch_get

--- docker-app/dockerize.sh
+++ docker-app/dockerize.sh
@@ -3,6 +3,6 @@ cp ../../logo.png logo.png
 cp ../../logo.png ./src/app/components/logo.png
 docker build -t freedomgpt/freedomgpt .
 docker run -d -p 8889:8889 freedomgpt/freedomgpt
-rm ../src/fetch_pdbs.js
+rm ../src/fetch_get.js
 rm logo.png 
 rm ./src/app/components/logo.png
